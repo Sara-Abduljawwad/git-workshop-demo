e05a95879981708014e027e3d57dc03af2bd43c9
Sara Abduljawwad <sararadwan220805@gmail.com>
2026-03-20 15:13:33 +0200

    add zero div error messg

--- calculator.py
+++ calculator.py
@@ -10,6 +10,8 @@ def multiply(a, b):
 return a * b 
 def divide(a, b): 
 """Return the quotient of a and b.""" 
+if b == 0: 
+raise ValueError("Cannot divide by zero")
 return a / b 
 if __name__ == "__main__": 
 print("Addition:    
